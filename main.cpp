8eb2fb9a80e88fd8df8ddfff71c640f711233226
Lissee <72330188+Lissee@users.noreply.github.com>
2020-10-11 20:07:48 +1000

    Update main.cpp
    Commit 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int X, Y;
+
+    cin >> X;
+    cin >> Y;
+    cout << "Lose! Result: " << Result;
+ 
     return 0;
-    std::cout << "Liss" << std::endl;
 }
